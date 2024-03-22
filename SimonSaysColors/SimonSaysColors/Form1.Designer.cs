3ed84bc75b1e3484bc9619a5925ce05c690b24dd
Lucas Martin Alvarez Bisordi <129774135+Alvarez-Bisordi-Lucas-Martin@users.noreply.github.com>
2024-03-22 03:39:18 -0300

    Add files via upload

---
+++ SimonSaysColors/SimonSaysColors/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SimonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonGreen = new System.Windows.Forms.Button();
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.buttonYellow = new System.Windows.Forms.Button();
+            this.buttonBlue = new System.Windows.Forms.Button();
+            this.buttonIniciar = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonGreen
+            // 
+            this.buttonGreen.BackColor = System.Drawing.Color.Green;
+            this.buttonGreen.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGreen.ForeColor = System.Drawing.Color.White;
+            this.buttonGreen.Location = new System.Drawing.Point(144, 43);
+            this.buttonGreen.Name = "buttonGreen";
+            this.buttonGreen.Size = new System.Drawing.Size(239, 199);
+            this.buttonGreen.TabIndex = 0;
+            this.buttonGreen.Text = "VERDE";
+            this.buttonGreen.UseVisualStyleBackColor = false;
+            this.buttonGreen.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.BackColor = System.Drawing.Color.Red;
+            this.buttonRed.Font = new System.Drawing.Font("Showcard Gothic", 12F);
+            this.buttonRed.ForeColor = System.Drawing.Color.White;
+            this.buttonRed.Location = new System.Drawing.Point(144, 248);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(239, 199);
+            this.buttonRed.TabIndex = 1;
+            this.buttonRed.Text = "ROJO";
+            this.buttonRed.UseVisualStyleBackColor = false;
+            this.buttonRed.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonYellow
+            // 
+            this.buttonYellow.BackColor = System.Drawing.Color.Yellow;
+            this.buttonYellow.Font = new System.Drawing.Font("Showcard Gothic", 12F);
+            this.buttonYellow.ForeColor = System.Drawing.Color.White;
+            this.buttonYellow.Location = new System.Drawing.Point(389, 248);
+            this.buttonYellow.Name = "buttonYellow";
+            this.buttonYellow.Size = new System.Drawing.Size(239, 199);
+            this.buttonYellow.TabIndex = 2;
+            this.buttonYellow.Text = "AMARILLO";
+            this.buttonYellow.UseVisualStyleBackColor = false;
+            this.buttonYellow.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonBlue
+            // 
+            this.buttonBlue.BackColor = System.Drawing.Color.Blue;
+            this.buttonBlue.Font = new System.Drawing.Font("Showcard Gothic", 12F);
+            this.buttonBlue.ForeColor = System.Drawing.Color.White;
+            this.buttonBlue.Location = new System.Drawing.Point(389, 43);
+            this.buttonBlue.Name = "buttonBlue";
+            this.buttonBlue.Size = new System.Drawing.Size(239, 199);
+            this.buttonBlue.TabIndex = 3;
+            this.buttonBlue.Text = "AZUL";
+            this.buttonBlue.UseVisualStyleBackColor = false;
+            this.buttonBlue.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonIniciar
+            // 
+            this.buttonIniciar.Font = new System.Drawing.Font("Showcard Gothic", 12F);
+            this.buttonIniciar.Location = new System.Drawing.Point(144, 3);
+            this.buttonIniciar.Name = "buttonIniciar";
+            this.buttonIniciar.Size = new System.Drawing.Size(484, 34);
+            this.buttonIniciar.TabIndex = 4;
+            this.buttonIniciar.Text = "INICIAR";
+            this.buttonIniciar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonIniciar);
+            this.Controls.Add(this.buttonBlue);
+            this.Controls.Add(this.buttonYellow);
+            this.Controls.Add(this.buttonRed);
+            this.Controls.Add(this.buttonGreen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGreen;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.Button buttonYellow;
+        private System.Windows.Forms.Button buttonBlue;
+        private System.Windows.Forms.Button buttonIniciar;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ SimonSaysColors/SimonSaysColors/Form1.cs
@@ -0,0 +1,155 @@
+﻿/* INTEGRANTES
+ Alvarez Lucas (26929)
+ Enora Joel (27092)
+ Tande Matías (27148)
+ Zarate Franco (27098)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SimonGame
+{
+    public partial class Form1 : Form
+    {
+        //Lista de botones
+        private List<Button> buttonList;
+        //Patron de colores
+        private List<int> patron;
+        //Indice actual en el patron
+        private int patronIndex;
+        //Generador de numeros aleatorios
+        private Random random;
+        //Indica si es el turno del jugador
+        private bool playerTurn;
+        //Indica si el juego ha terminado
+        private bool gameOver;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Agregar evento Click al boton Iniciar
+            buttonIniciar.Click += buttonIniciar_Click;
+
+            //Inicializar variables
+            buttonList = new List<Button> { buttonGreen, buttonRed, buttonBlue, buttonYellow };
+            patron = new List<int>();
+            patronIndex = 0;
+            random = new Random();
+            playerTurn = false;
+            gameOver = false;
+
+            //Intervalo de 1 segundo entre colores
+            timer.Interval = 1000;
+            timer.Tick += MostarSiguienteColor;
+        }
+
+        //Metodo para manejar el evento Click del botón Iniciar
+        private void buttonIniciar_Click(object sender, EventArgs e)
+        {
+            if (!playerTurn)
+            {
+                StartGame();
+            }
+        }
+
+        //Metodo para manejar el evento Click de los botones de colores
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (playerTurn && !gameOver)
+            {
+                Button clickedButton = (Button)sender;
+                int buttonIndex = buttonList.IndexOf(clickedButton);
+
+                if (buttonIndex == patron[patronIndex])
+                {
+                    patronIndex++;
+
+                    if (patronIndex == patron.Count)
+                    {
+                        playerTurn = false;
+                        patronIndex = 0;
+                        AgregarColor();
+                        MostrarPatron();
+                    }
+                }
+                else
+                {
+                    GameOver();
+                }
+            }
+        }
+
+        //Metodo para iniciar el juego
+        private void StartGame()
+        {
+            patron.Clear();
+            patronIndex = 0;
+            playerTurn = false;
+            gameOver = false;
+
+            AgregarColor();
+            MostrarPatron();
+        }
+
+        //Metodo para agregar un nuevo color al patron
+        private void AgregarColor()
+        {
+            int randomIndex = random.Next(0, buttonList.Count);
+            patron.Add(randomIndex);
+        }
+
+        //Metodo para mostrar el patron de colores al jugador
+        private void MostrarPatron()
+        {
+            playerTurn = false;
+            timer.Start();
+        }
+
+        //Metodo para mostrar el siguiente color del patron
+        private void MostarSiguienteColor(object sender, EventArgs e)
+        {
+            timer.Stop();
+
+            if (patronIndex < patron.Count)
+            {
+                int buttonIndex = patron[patronIndex];
+                Button button = buttonList[buttonIndex];
+                ResaltarButton(button);
+                patronIndex++;
+                timer.Start();
+            }
+            else
+            {
+                playerTurn = true;
+                patronIndex = 0;
+                MessageBox.Show("¡Es tu turno! Repite la secuencia de colores.");
+            }
+        }
+
+        //Metodo para resaltar un boton especifico durante un tiempo determinado
+        private void ResaltarButton(Button button)
+        {
+            Color originalColor = button.BackColor;
+            button.BackColor = Color.White;
+            Refresh();
+            //Mantener el color iluminado durante 0.5 segundos
+            Thread.Sleep(500);
+            button.BackColor = originalColor;
+            Refresh();
+            //Esperar 0.5 segundos antes de pasar al siguiente color
+            Thread.Sleep(500);
+        }
+
+        //Metodo para manejar el error del jugador en la carga de la secuencia
+        private void GameOver()
+        {
+            gameOver = true;
+            MessageBox.Show("¡Has perdido! Intentalo de nuevo.");
+            StartGame();
+        }
+    }
+}
